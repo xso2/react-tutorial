5389bfd8d4b704968242732f4d89c341dc5ea9f3
xso2 <odaken1002@gmail.com>
2022-08-04 00:38:18 +0900

    map関数の記述

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
+
 function App() {
+  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+
   return (
     <div>
-      <TodoList />
+      <TodoList todos={todos}/>
+      <input type = "text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TodoList = ({ todos }) => {
+  return todos.map((todo) => <Todo />);
+};
+
+export default TodoList
