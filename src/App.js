251eff1e636830daba6c37329fefab15c74f2b70
xso2 <odaken1002@gmail.com>
2022-08-04 04:36:23 +0900

    フィルター関数の記載

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -12,18 +13,25 @@ function App() {
     //タスクを追加する
     const name = todoNameRef.current.value; 
     setTodos((prevTodos) => {
-      return [...prevTodos, {id: "1", name: name, completed: false }]
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false }]
     });
     todoNameRef.current.value = null;
   };
 
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
   return (
     <div>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type = "text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
-      <div>残りのタスク:0</div>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
